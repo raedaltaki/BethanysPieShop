d2929e77600afd0fc963388792bd5ad615ff05bd
raedaltaki <raed.simon@gmail.com>
2022-02-17 23:09:25 -0500

    add db

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 using BethanysPieShop.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IPieRepository, MockPieRepository>();
 builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
 //builder.Services.AddTransient
