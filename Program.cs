3de214aa18780a4e85e0ee2ad3d7397d1a6e9c86
raedaltaki <raed.simon@gmail.com>
2022-02-25 01:37:31 -0500

    add order model, view and controllers

--- Program.cs
+++ Program.cs
@@ -5,8 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddScoped<IPieRepository, PieRepository>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+
 builder.Services.AddScoped<ShoppingCart>(sp => ShoppingCart.GetCart(sp));
 //builder.Services.AddScoped<IPieRepository, MockPieRepository>();
 //builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
